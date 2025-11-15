097225ed0cd6d0b6d89589c5191f5921da200029
ElSm <ems727@lehigh.edu>
2025-11-15 13:42:58 -0500

    add pages

---
+++ final_project/src/layout.jsx
@@ -0,0 +1,18 @@
+import {Link, Outlet} from 'react-router-dom'
+
+export default function Layout() {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to ="/">Home</Link>
+                    </li>
+                    
+                </ul>
+            </nav>
+            <hr/>
+            <Outlet/>
+        </div>
+    )
+}
