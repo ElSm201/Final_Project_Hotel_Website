363f1a14707e3975c831e595b00d87a278d145d6
ElSm <ems727@lehigh.edu>
2025-11-17 15:11:58 -0500

    new branch

--- final_project/src/pages/home.jsx
+++ final_project/src/pages/home.jsx
@@ -4,4 +4,19 @@ import { Link } from 'react-router-dom';
 
 export default function Home(){
     
+    return(
+        <div>
+            <div className = "nav">
+                <ul>
+                    <Link to="/home">Home</Link>
+                    <Link to="/rooms">Book a Room</Link>
+                    <Link to="/reviews">Resturants nearby</Link>
+                    <Link to="/login">Employee Login</Link>
+                </ul>
+            </div>
+            <style jsx>{
+
+            }</style>
+        </div>
+    )
 }
