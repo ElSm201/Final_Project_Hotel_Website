aedd972761a39c0b72cd3baf66661204e7ec8c19
ElSm <ems727@lehigh.edu>
2025-11-25 19:32:49 -0500

    added props for booking

--- final_project/src/App.jsx
+++ final_project/src/App.jsx
@@ -7,17 +7,25 @@ import Rooms from './pages/rooms'
 import Login from './pages/login'
 import Employees from './pages/employees';
 import Booking from './pages/booking'
+import React, {useState} from 'react';
+
+
 
 function App() {
+    const [bookingSettings, setBookingSettings] = useState({
+    roomType: '',
+    roomPrice: '',
+    travelDates: {checkIn: '', checkOut: ''},
+  });
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/rooms" element={<Rooms/>} /> 
+        <Route path="/rooms" element={<Rooms setBookingSettings={setBookingSettings}/>} /> 
         <Route path="/reviews" element={<Reviews />} /> 
         <Route path="/login" element={<Login/>} />
         <Route path="/employees" element={<Employees/>} />
-        <Route path="/booking" element={<Booking/>} />
+        <Route path="/booking" element={<Booking bookingSettings={bookingSettings}/>} />
         <Route path="*" element={<div>Page Not Found</div>} />
       </Routes>
     </BrowserRouter>
--- final_project/src/pages/booking.jsx
+++ final_project/src/pages/booking.jsx
@@ -3,23 +3,26 @@ import {Box,TextField,Button} from '@mui/material';
 import {Link } from 'react-router-dom'
 import {useForm} from 'react-hook-form'
 
-export default function Booking() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-
-
+export default function Booking({bookingSettings}) {
+ 
+  const { roomType, roomPrice, travelDates } = bookingSettings;
+  const { checkIn, checkOut } = travelDates;
   const {register, handleSubmit} = useForm()
    
 
   const onSubmit = (e) => { //nothing is really gonna happen for now
-    console.log('Name:', name);
-    console.log('Email:', email);
+    console.log(e)
     alert('Booking submitted!');
   };
-
+//how to do math with dates in java script? https://medium.com/@selbekk/math-with-dates-in-javascript-2b0ddcee63f
+const checkInDate = new Date(checkIn);
+const checkOutDate = new Date(checkOut);
+const timeDifference = checkOutDate.getTime() - checkInDate.getTime();
+//and then to convert to days and multiple by price
 
   const customerName = register('Customer Name', { required: 'Customer Name is required' })
   const customerEmail = register('Customer Email', { required: 'Customer Email is required' })
+  const customerPhone = register('Customer Email', { required: 'Customer Phone Number is required' })
 
   return (
     <div>
@@ -31,22 +34,32 @@ export default function Booking() {
       </ul>
    
       <h2>Complete Your Booking!</h2>
+      <h3>Room Type: {roomType}</h3>
+      <h3>Room Price: {roomPrice}</h3>
+      <h3>Travel Dates: {checkIn} to {checkOut}</h3>
+  
       <Box
         component="form"
         onSubmit={handleSubmit(onSubmit)}
         sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}
       >
         <TextField
-          label = "customerName"
+          label = "Customer Name"
           name = {customerName.name}
           onChange = {customerName.onChange}
           inputRef = {customerName.ref}
           required/>
         <TextField
-          label = "custpmerEmail"
+          label = "Customer Email"
           name = {customerEmail.name}
           onChange = {customerEmail.onChange}
           inputRef = {customerEmail.ref}
+          required />
+          <TextField
+          label = "Customer Phone Number"
+          name = {customerPhone.name}
+          onChange = {customerPhone.onChange}
+          inputRef = {customerPhone.ref}
           required />
         <Button type="submit" variant="contained">
           Submit Booking
