7547f9ae8465734c08c1c6304b936496b541d1fe
RReeyah Mabry <ram227@lehigh.edu>
2025-12-01 13:02:07 -0500

    made corrections to some routes

--- final_project/db/postgres.js
+++ final_project/db/postgres.js
@@ -1,15 +1,17 @@
 import pg from 'pg'
-const { Client } = pg
+const { Pool } = pg
 
-const client = new Client({
-  host: process.env.POSTGRES_HOST,
-  port: Number(process.env.POSTGRES_PORT),
-  database: process.env.POSTGRES_DBNAME,
-  user: process.env.POSTGRES_USERNAME,
-  password: process.env.POSTGRES_PASSWORD,
-  ssl: {
-    rejectUnauthorized: false, // Needed for Supabase SSL
-  },
+const client = new Pool({
+  // host: process.env.POSTGRES_HOST,
+  // port: Number(process.env.POSTGRES_PORT),
+  // database: process.env.POSTGRES_DBNAME,
+  // user: process.env.POSTGRES_USERNAME,
+  // password: process.env.POSTGRES_PASSWORD,
+  // ssl: {
+  //   rejectUnauthorized: false, // Needed for Supabase SSL
+  // }
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 })
 
 client.connect()
--- final_project/routes/guest.js
+++ final_project/routes/guest.js
@@ -13,6 +13,7 @@ const room = 'rooms'
 
 
 const guestRoutes = (app) => {
+    console.log("printed")
     /**
      * Post /api/booking
      * routes purpose is to establishing which rooms are available that match the type indicated 
@@ -21,16 +22,18 @@ const guestRoutes = (app) => {
    app.post('/api/booking', async(req,res)=> {
     try{
         let{name, email, type,check_in, check_out} = req.body; //have to get the guest_id
+        const checkInISO = new Date(check_in).toISOString();
+        const checkOutISO = new Date(check_out).toISOString();
         //getting the specified type of room requested
         let room_query = `Select room_num 
                             FROM ${room} r
                             WHERE r.type = $1
                             AND r.room_num NOT IN (
                                 SELECT room_id FROM ${reserve}
-                                WHERE ($2 < check_out AND $3 > check_in)
+                                WHERE ($2::timestamptz < check_out AND $3::timestamptz > check_in)
                             ) `
-        const {rows: roomRows} = await query(room_query,[type])
-        if(rows.length === 0){
+        const {rows: roomRows} = await query(room_query,[type,checkInISO, checkOutISO])
+        if(roomRows.length === 0){
             throw new Error("no rooms of that type")
         }
         //picking a random room from the list to assign to the guest upon making reservation
@@ -38,6 +41,7 @@ const guestRoutes = (app) => {
         /**if a guest is already in the system we grab the assigned guest id and reuse it. Else we add the guest
         * we add guest to guest_info, get the assigned guest id and use that for reservation
         */
+       console.log("error here")
         let reserve_query = `
             With guest As(
                 Insert into ${contact} (name,email)
@@ -56,7 +60,8 @@ const guestRoutes = (app) => {
             select guest_id, $3,'confirmed',$4,$5
             from final_guest
             `
-        const done = await query(reserve_query,[name,email,randRow.room_num,check_in, check_out])
+        const done = await query(reserve_query,[name,email,randRow.room_num,checkInISO, checkOutISO])
+        res.status(201).json({ message: "Booking confirmed", room: randRow.room_num });
     }catch(err){
         res.status(500).json({error: 'internal server error'})
     }
@@ -67,17 +72,23 @@ const guestRoutes = (app) => {
     * Booking is meant to check that there are available room types 
     * for the check-in/out date specified by potential guest
    */
-   app.get('/api/availability', async(req,res) =>{
+   app.post('/api/availability', async(req,res) =>{
         try{
             let {type, check_in, check_out} = req.body
+            if (!type || !check_in || !check_out) {
+                return res.status(400).json({ error: 'Missing required fields' });
+            }
+            const checkInISO = new Date(check_in).toISOString();
+            const checkOutISO = new Date(check_out).toISOString();
+            console.log("good")
             const available =  `Select room_num 
                                 FROM ${room} r
                                 WHERE r.type = $1
                                 AND r.room_num NOT IN (
                                     SELECT room_id FROM ${reserve}
-                                    WHERE ($2 < check_out AND $3 > check_in)
+                                    WHERE ($2::timestamptz < check_out AND $3::timestamptz > check_in)
                                 )`
-            const {rows} = await query(available, [type, check_in, check_out])
+            const {rows} = await query(available, [type, checkInISO, checkOutISO])
             if(rows.length === 0){
                 return res.status(404).json({error: 'No available rooms'})
             }
